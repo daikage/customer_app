3b8c2886d6ae671f25cd07e80f3aaf9eb169ca29
daikage <30565500+daikage@users.noreply.github.com>
2026-08-11 17:22:48 +0100

    fbf

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'dart:convert' as dart_convert;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'providers/auth_provider.dart';
-import 'providers/ride_provider.dart';
 import 'services/realtime_bindings.dart';
-import 'services/reverb_service.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/splash_screen.dart';
